df8327a159b702103f5adc6001d568d05e8182cd
Jack Liu <jackliu@microsoft.com>
2020-03-11 09:32:30 -0700

    Temp

--- src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/1.sql
+++ src/Microsoft.Health.Fhir.SqlServer/Features/Schema/Migrations/1.sql
@@ -8,19 +8,25 @@
 
 DECLARE @sql nvarchar(max) =''
 
-SELECT @sql = @sql + 'DROP PROCEDURE ' + name + '; '
+SELECT @sql = @sql + 'DROP PROCEDURE dicom.' + name + '; '
 FROM sys.procedures
 
-SELECT @sql = @sql + 'DROP TABLE ' + name + '; '
+SELECT @sql = @sql + 'DROP TABLE dicom.' + name + '; '
 FROM sys.tables
 
-SELECT @sql = @sql + 'DROP TYPE ' + name + '; '
+SELECT @sql = @sql + 'DROP TYPE dicom.' + name + '; '
 FROM sys.table_types
 
-SELECT @sql = @sql + 'DROP SEQUENCE ' + name + '; '
+SELECT @sql = @sql + 'DROP SEQUENCE dicom.' + name + '; '
 FROM sys.sequences
 
+SELECT @sql
+
 EXEC(@sql)
+
+GO
+
+DROP SCHEMA dicom
 
 GO
 
@@ -136,6 +142,13 @@ CREATE TABLE dicom.Instance (
 	CreatedDate DATETIME2(7) NOT NULL
 )
 
+CREATE UNIQUE CLUSTERED INDEX IXC_Instance ON dicom.Instance
+(
+    StudyInstanceUID,
+    SeriesInstanceUID,
+    SOPInstanceUID
+)
+
 --Table containing normalized standard Study tags
 CREATE TABLE dicom.StudyMetadataCore (
 	--Key
@@ -150,7 +163,7 @@ CREATE TABLE dicom.StudyMetadataCore (
 	ReferringPhysicianName NVARCHAR(64) NULL,
 	StudyDate DATE NULL,
 	StudyDescription NVARCHAR(64) NULL,
-	AccessionNumer NVARCHAR(16) NULL,
+	AccessionNumer NVARCHAR(16)  NULL,
 )
 
 --Table containing normalized standard Series tags
@@ -164,4 +177,154 @@ CREATE TABLE dicom.SeriesMetadataCore (
 	Modality NVARCHAR(16) NULL,
 	PerformedProcedureStepStartDate DATE NULL
 ) 
+GO
+
+/*************************************************************
+    Sequence for generating unique 12.5ns "tick" components that are added
+    to a base ID based on the timestamp to form a unique resource surrogate ID
+**************************************************************/
+
+CREATE SEQUENCE dicom.MetadataIdSequence
+        AS BIGINT
+        START WITH 0
+        INCREMENT BY 1
+        MINVALUE 0
+        NO CYCLE
+        CACHE 1000000
+GO
+
+/*************************************************************
+    Stored procedures for adding an instance.
+**************************************************************/
+--
+-- STORED PROCEDURE
+--     AddInstance
+--
+-- DESCRIPTION
+--     adds a DICOM instance
+--
+-- PARAMETERS
+--     @studyInstanceUid
+--         * A bigint to which a value between [0, 80000) is added, forming a unique ResourceSurrogateId.
+--         * This value should be the current UTC datetime, truncated to millisecond precision, with its 100ns ticks component bitshifted left by 3.
+--     @resourceTypeId
+--         * The ID of the resource type (See ResourceType table)
+--     @resourceid
+--         * The resource ID (must be the same as the in the resource itself)
+--     @allowCreate
+--         * If false, an error is thrown if the resource does not already exist
+--     @isDeleted
+--         * Whether this resource marks the resource as deleted
+--     @updatedDateTime
+--         * The last modified time in the resource
+--     @keepHistory
+--         * Whether the existing version of the resource should be preserved
+--     @requestMethod
+--         * The HTTP method/verb used for the request
+--     @rawResource
+--         * A compressed UTF16-encoded JSON document
+--     @resourceWriteClaims
+--         * Claims on the principal that performed the write
+--     @compartmentAssignments
+--         * Compartments that the resource is part of
+--     @referenceSearchParams
+--         * Extracted reference search params
+--     @tokenSearchParams
+--         * Extracted token search params
+--     @tokenTextSearchParams
+--         * The text representation of extracted token search params
+--     @stringSearchParams
+--         * Extracted string search params
+--     @numberSearchParams
+--         * Extracted number search params
+--     @quantitySearchParams
+--         * Extracted quantity search params
+--     @uriSearchParams
+--         * Extracted URI search params
+--     @dateTimeSearchParms
+--         * Extracted datetime search params
+--     @referenceTokenCompositeSearchParams
+--         * Extracted reference$token search params
+--     @tokenTokenCompositeSearchParams
+--         * Extracted token$token tokensearch params
+--     @tokenDateTimeCompositeSearchParams
+--         * Extracted token$datetime search params
+--     @tokenQuantityCompositeSearchParams
+--         * Extracted token$quantity search params
+--     @tokenStringCompositeSearchParams
+--         * Extracted token$string search params
+--     @tokenNumberNumberCompositeSearchParams
+--         * Extracted token$number$number search params
+--
+-- RETURN VALUE
+--         The version of the resource as a result set. Will be empty if no insertion was done.
+--
+
+CREATE PROCEDURE dicom.AddInstance
+    @studyInstanceUID VARCHAR(64),
+    @seriesInstanceUID VARCHAR(64),
+    @sopInstanceUID VARCHAR(64),
+    @patientId VARCHAR(64) = NULL,
+    @patientName VARCHAR(64),
+    @referringPhysicianName VARCHAR(64) = NULL,
+    @studyDate DATE = NULL,
+    @studyDescription VARCHAR(64) = NULL,
+    @accessionNumber VARCHAR(64) = NULL,
+    @modality VARCHAR(16) = NULL,
+    @performedProcedureStepStartDate DATE = NULL
+AS
+    SET NOCOUNT ON
+
+    SET XACT_ABORT ON
+    BEGIN TRANSACTION
+
+    DECLARE @currentDate DATETIME2(7) = GETUTCDATE()
+    DECLARE @existingStatus TINYINT
+
+    SELECT @existingStatus = Status
+    FROM dicom.Instance WITH (UPDLOCK, HOLDLOCK)
+    WHERE StudyInstanceUID = @studyInstanceUID AND SeriesInstanceUID = @seriesInstanceUID AND SOPInstanceUID = @sopInstanceUID
+
+    IF (@existingStatus IS NULL) BEGIN
+        -- The instance does not exist, insert it.
+        INSERT INTO dicom.Instance
+            (StudyInstanceUID, SeriesInstanceUID, SOPInstanceUID, Watermark, Status, LastStatusUpdatesDate, CreatedDate)
+        VALUES
+            (@studyInstanceUID, @seriesInstanceUID, @sopInstanceUID, 0, 0, @currentDate, @currentDate)
+
+        -- Update the study metadata if needed.
+        DECLARE @metadataId BIGINT
+
+        SELECT @metadataId = ID
+        FROM dicom.StudyMetadataCore WITH (NOLOCK)
+        WHERE StudyInstanceUID = @studyInstanceUID
+
+        IF (@metadataId IS NULL) BEGIN
+            SET @metadataId = NEXT VALUE FOR dicom.MetadataIdSequence
+
+            INSERT INTO dicom.StudyMetadataCore
+                (ID, StudyInstanceUID, Version, PatientID, PatientName, ReferringPhysicianName, StudyDate, StudyDescription, AccessionNumer)
+            VALUES
+                (@metadataId, @studyInstanceUID, 0, @patientId, @patientName, @referringPhysicianName, @studyDate, @studyDescription, @accessionNumber)
+        END
+        --ELSE BEGIN
+          -- TODO: handle the versioning
+        --END
+
+        IF NOT EXISTS (SELECT * FROM dicom.SeriesMetadataCore WHERE ID = @metadataId AND SeriesInstanceUID = @seriesInstanceUID) BEGIN
+            INSERT INTO dicom.SeriesMetadataCore
+                (ID, SeriesInstanceUID, Version, Modality, PerformedProcedureStepStartDate)
+            VALUES
+                (@metadataId, @seriesInstanceUID, 0, @modality, @performedProcedureStepStartDate)
+        END
+        --ELSE BEGIN
+          -- TODO: handle the versioning
+        --END
+    END
+    ELSE BEGIN
+        -- TODO: handle the conflict case
+        THROW 50409, 'Instance already exists', 1;
+    END
+
+    COMMIT TRANSACTION
 GO
