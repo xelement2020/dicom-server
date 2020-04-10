ea851d6633c1e2d5a9464a3c67ee49a071c56fe7
Jack Liu <jackliu@microsoft.com>
2020-04-10 09:47:54 -0700

    Temp

---
+++ src/Microsoft.Health.Dicom.Core.UnitTests/Features/Store/DicomInstanceEntryProcessorTests.cs
@@ -0,0 +1,119 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Dicom;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Health.Dicom.Core.Features.Store;
+using Microsoft.Health.Dicom.Core.Features.Store.Entries;
+using Microsoft.Health.Dicom.Core.Messages.Store;
+using Microsoft.Health.Dicom.Tests.Common;
+using NSubstitute;
+using Xunit;
+
+namespace Microsoft.Health.Dicom.Core.UnitTests.Features.Store
+{
+    public class DicomInstanceEntryProcessorTests
+    {
+        private static readonly CancellationToken DefaultCancellationToken = new CancellationTokenSource().Token;
+
+        private readonly DicomDataset _dicomDataset1 = Samples.CreateRandomInstanceDataset(
+            studyInstanceUid: "1",
+            seriesInstanceUid: "2",
+            sopInstanceUid: "3",
+            sopClassUid: "4");
+
+        private readonly DicomDataset _dicomDataset2 = Samples.CreateRandomInstanceDataset(
+            studyInstanceUid: "10",
+            seriesInstanceUid: "11",
+            sopInstanceUid: "12",
+            sopClassUid: "13");
+
+        private readonly Func<DicomStoreResponseBuilder> _dicomStoreResponseBuilderFactory;
+        private readonly IDicomStoreService _dicomStoreService = Substitute.For<IDicomStoreService>();
+        private readonly DicomInstanceEntryProcessor _dicomInstanceEntryProcessor;
+
+        public DicomInstanceEntryProcessorTests()
+        {
+            _dicomStoreResponseBuilderFactory = () => new DicomStoreResponseBuilder(new MockUrlResolver());
+
+            _dicomInstanceEntryProcessor = new DicomInstanceEntryProcessor(
+                _dicomStoreResponseBuilderFactory,
+                _dicomStoreService,
+                NullLogger<DicomInstanceEntryProcessor>.Instance);
+        }
+
+        [Fact]
+        public async Task GivenNullDicomInstanceEntries_WhenProcessed_ThenNoContentShouldBeReturned()
+        {
+            DicomStoreResponse response = await _dicomInstanceEntryProcessor.ProcessAsync(
+                dicomInstanceEntries: null,
+                requiredStudyInstanceUid: null,
+                cancellationToken: DefaultCancellationToken);
+
+            Assert.NotNull(response);
+            Assert.Equal((int)HttpStatusCode.NoContent, response.StatusCode);
+            Assert.Null(response.Dataset);
+        }
+
+        [Fact]
+        public async Task GivenEmptyDicomInstanceEntries_WhenProcessed_ThenNoContentShouldBeReturned()
+        {
+            DicomStoreResponse response = await _dicomInstanceEntryProcessor.ProcessAsync(
+                dicomInstanceEntries: new IDicomInstanceEntry[0],
+                requiredStudyInstanceUid: null,
+                cancellationToken: DefaultCancellationToken);
+
+            Assert.NotNull(response);
+            Assert.Equal((int)HttpStatusCode.NoContent, response.StatusCode);
+            Assert.Null(response.Dataset);
+        }
+
+        [Fact]
+        public async Task GivenAValidDicomInstanceEntry_WhenProcessed_ThenSuccessfulEntryShouldBeAdded()
+        {
+            IDicomInstanceEntry dicomInstanceEntry = Substitute.For<IDicomInstanceEntry>();
+
+            dicomInstanceEntry.GetDicomDatasetAsync(DefaultCancellationToken).Returns(_dicomDataset1);
+
+            DicomStoreResponse response = await _dicomInstanceEntryProcessor.ProcessAsync(
+                new[] { dicomInstanceEntry },
+                requiredStudyInstanceUid: null,
+                cancellationToken: DefaultCancellationToken);
+
+            Assert.NotNull(response);
+            Assert.Equal((int)HttpStatusCode.OK, response.StatusCode);
+            Assert.NotNull(response.Dataset);
+
+            ValidationHelpers.ValidateReferencedSopSequence(
+                response.Dataset,
+                ("3", "/1/2/3", "4"));
+        }
+
+        [Fact]
+        public async Task GiveAnInvalidDicomDataset_WhenProcessed_ThenFailedEntryShouldBeAdded()
+        {
+            IDicomInstanceEntry dicomInstanceEntry = Substitute.For<IDicomInstanceEntry>();
+
+            dicomInstanceEntry.GetDicomDatasetAsync(DefaultCancellationToken).Returns<DicomDataset>(_ => throw new Exception());
+
+            DicomStoreResponse response = await _dicomInstanceEntryProcessor.ProcessAsync(
+                new[] { dicomInstanceEntry },
+                requiredStudyInstanceUid: null,
+                cancellationToken: DefaultCancellationToken);
+
+            Assert.NotNull(response);
+            Assert.Equal((int)HttpStatusCode.Conflict, response.StatusCode);
+            Assert.NotNull(response.Dataset);
+
+            ValidationHelpers.ValidateFailedSopSequence(
+                response.Dataset,
+                (null, null, TestConstants.ProcessingFailureReasonCode));
+        }
+    }
+}
--- src/Microsoft.Health.Dicom.Tests.Common/ValidationHelpers.cs
+++ src/Microsoft.Health.Dicom.Tests.Common/ValidationHelpers.cs
@@ -48,5 +48,48 @@ namespace Microsoft.Health.Dicom.Tests.Common
                     dataset.GetSingleValue<string>(DicomTag.RetrieveURL));
             }
         }
+
+        public static void ValidateReferencedSopSequence(DicomDataset actualDicomDataset, params (string SopInstanceUid, string RetrieveUri, string SopClassUid)[] expectedValues)
+        {
+            Assert.True(actualDicomDataset.TryGetSequence(DicomTag.ReferencedSOPSequence, out DicomSequence sequence));
+            Assert.Equal(expectedValues.Length, sequence.Count());
+
+            for (int i = 0; i < expectedValues.Length; i++)
+            {
+                DicomDataset actual = sequence.ElementAt(i);
+
+                Assert.Equal(expectedValues[i].SopInstanceUid, actual.GetSingleValueOrDefault<string>(DicomTag.ReferencedSOPInstanceUID));
+                Assert.Equal(expectedValues[i].RetrieveUri, actual.GetSingleValueOrDefault<string>(DicomTag.RetrieveURL));
+                Assert.Equal(expectedValues[i].SopClassUid, actual.GetSingleValueOrDefault<string>(DicomTag.ReferencedSOPClassUID));
+            }
+        }
+
+        public static void ValidateFailedSopSequence(DicomDataset actualDicomDataset, params (string SopInstanceUid, string SopClassUid, ushort FailureReason)[] expectedValues)
+        {
+            Assert.True(actualDicomDataset.TryGetSequence(DicomTag.FailedSOPSequence, out DicomSequence sequence));
+            Assert.Equal(expectedValues.Length, sequence.Count());
+
+            for (int i = 0; i < expectedValues.Length; i++)
+            {
+                DicomDataset actual = sequence.ElementAt(i);
+
+                ValidateNullOrCorrectValue(expectedValues[i].SopInstanceUid, actual, DicomTag.ReferencedSOPInstanceUID);
+                ValidateNullOrCorrectValue(expectedValues[i].SopClassUid, actual, DicomTag.ReferencedSOPClassUID);
+
+                Assert.Equal(expectedValues[i].FailureReason, actual.GetSingleValueOrDefault<ushort>(DicomTag.FailureReason));
+            }
+
+            void ValidateNullOrCorrectValue(string expectedValue, DicomDataset actual, DicomTag dicomTag)
+            {
+                if (expectedValue == null)
+                {
+                    Assert.False(actual.TryGetSingleValue(dicomTag, out string _));
+                }
+                else
+                {
+                    Assert.Equal(expectedValue, actual.GetSingleValueOrDefault<string>(dicomTag));
+                }
+            }
+        }
     }
 }
